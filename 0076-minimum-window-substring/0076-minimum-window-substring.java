fd4ba193d22b78389c8508e350b16dac89c7bc55
ChandraKiran-30 <moonlightt6253@gmail.com>
2026-05-26 16:29:38 +0530

    Time: 21 ms (31.66%), Space: 47.6 MB (35.16%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,31 +1,31 @@
 class Solution {
     public String minWindow(String s, String t) {
-        if (t.equals(""))
+        if (t.length() == 0)
             return "";
         HashMap<Character, Integer> hmT = new HashMap<>();
         HashMap<Character, Integer> hmS = new HashMap<>();
+        int idx1 = -1, idx2 = -1, len = Integer.MAX_VALUE;
         for (int i = 0; i < t.length(); i++) {
             char ch = t.charAt(i);
             hmT.put(ch, hmT.getOrDefault(ch, 0) + 1);
         }
         int have = 0, need = hmT.size();
-        int idx1 = -1, idx2 = -1, len = Integer.MAX_VALUE;
-        int l = 0, r = 0;
+        int i = 0, j = 0;
         char str[] = s.toCharArray();
-        while (r < str.length) {
-            char ch = str[r];
+        while (j < str.length) {
+            char ch = str[j];
             if (hmT.containsKey(ch)) {
                 hmS.put(ch, hmS.getOrDefault(ch, 0) + 1);
                 if (hmS.get(ch).equals(hmT.get(ch)))
                     have++;
             }
             while (have == need) {
-                if ((r - l + 1) < len) {
-                    len = r - l + 1;
-                    idx1 = l;
-                    idx2 = r;
+                if ((j - i + 1) < len) {
+                    len = j - i + 1;
+                    idx1 = i;
+                    idx2 = j;
                 }
-                char c = str[l];
+                char c = str[i];
                 if (hmT.containsKey(c)) {
                     hmS.put(c, hmS.get(c) - 1);
                     if (hmS.get(c) < hmT.get(c))
@@ -33,12 +33,10 @@ class Solution {
                     if (hmS.get(c) == 0)
                         hmS.remove(c);
                 }
-                l++;
+                i++;
             }
-            r++;
+            j++;
         }
-        if (len == Integer.MAX_VALUE)
-            return "";
-        return s.substring(idx1, idx2 + 1);
+        return (len == Integer.MAX_VALUE) ? "" : s.substring(idx1, idx2 + 1);
     }
 }
